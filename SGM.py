fce115b5967593f2ad05d326f2c41983107d1c00
Yu Hsien Chiang <yuhsienchiang@Yus-MacBook-Pro.local>
2022-10-18 11:13:30 +1100

    implement SGM and optimise NCC algor

---
+++ SGM.py
@@ -0,0 +1,104 @@
+class SGM():
+    
+    def __init__(self, p1, p2, cost_map=None):
+        if cost_map is not None:
+            self.cost_map = cost_map
+        self.p1 = p1
+        self.p2 = p2
+    
+    def compute(self, cost_map=None):
+        
+        if cost_map is not None:
+            self.cost_map = cost_map
+        
+        H, W, D = self.cost_map.shape
+        
+        E_init = np.zeros((H, W, D))
+        E_init[0, :, :] = self.cost_map[0, :, :]
+        E_init[H-1, :, :] = self.cost_map[H-1, :, :]
+        E_init[:, 0, :] = self.cost_map[:, 0, :]
+        E_init[:, W-1, :] = self.cost_map[:, W-1, :]
+        
+        # Left to Right scan
+        #L2R
+        E_a1 = np.copy(E_init)
+        E_a1[:, 1:W] = 0
+        
+        #TL2BR
+        E_a5 = np.copy(E_init)
+        E_a5[1:H, 1:W] = 0
+        
+        #BL2TR
+        E_a8 = np.copy(E_init)
+        E_a8[0:H-1, 1:W] = 0
+        
+        for w in range(1, W):
+            E_a1[:, w, :] = self.cost_map[:, w, :] + self.linear_cost_arggregate(E_pre=E_a1[:, w-1, :])
+            
+            c_m = self.cost_map[:, w, :]
+            c_m[1:H, :] = c_m[1:H, :] + self.linear_cost_arggregate(E_pre=E_a5[:, w-1, :])[0: H-1, :]
+            E_a5[:, w, :] = c_m
+            
+            c_m = self.cost_map[:, w, :]
+            c_m[0:H-1, :] = c_m[0:H-1, :] + self.linear_cost_arggregate(E_pre=E_a8[:, w-1, :])[1:H, :]
+            E_a8[:, w, :] = c_m
+        
+        
+        # Right to Left scan
+        #R2L
+        E_a2 = np.copy(E_init)
+        E_a2[:, 0:W-1] = 0
+        
+        #TR2BL
+        E_a6 = np.copy(E_init)
+        E_a6[1:H, 0:W-1] = 0
+        
+        #BR2TL
+        E_a7 = np.copy(E_init)
+        E_a7[0:H-1, 0:W-1] = 0
+        
+        for w in range(W-2, -1, -1):
+            E_a2[:, w, :] = self.cost_map[:, w, :] + self.linear_cost_arggregate(E_pre=E_a2[:, w+1, :])
+            
+            c_m = self.cost_map[:, w, :]
+            c_m[1:H, :] = c_m[1:H, :] + self.linear_cost_arggregate(E_pre=E_a6[:, w+1, :])[0:H-1, :]
+            E_a6[:, w, :] = c_m
+            
+            c_m = self.cost_map[:, w, :]
+            c_m[0:H-1, :] =c_m[0:H-1, :] + self.linear_cost_arggregate(E_pre=E_a7[:, w+1, :])[1:H, :]
+            E_a7[:, w, :] = c_m
+        
+        # Top to Bottom scan
+        #T2B
+        E_a3 = np.copy(E_init)
+        E_a3[1:H, :] = 0
+        for h in range(1, H):
+            E_a3[h, :, :] = self.cost_map[h, :, :] + self.linear_cost_arggregate(E_pre=E_a3[h-1, :, :])
+                    
+        # Bottom to Top scan
+        #B2T
+        E_a4 = np.copy(E_init)
+        E_a4[0:H-1, :] = 0
+        for h in range(H-2, -1, -1):
+            E_a4[h, :, :] = self.cost_map[h, :, :] + self.linear_cost_arggregate(E_pre=E_a4[h+1, :, :])
+               
+        E = np.array(E_a1 + E_a2 + E_a3 + E_a4 + E_a2 + E_a6 + E_a7 + E_a8, dtype=np.int32)
+        
+        disparity_map = np.argmin(E, axis=2)
+        self.disparity_map = disparity_map
+        
+        return disparity_map
+    
+    
+    def linear_cost_arggregate(self, E_pre):
+        R, D = E_pre.shape
+        
+        d_plus = np.full((R,D), np.inf)
+        d_plus[:, 1:D] = E_pre[:, 0:D-1] + self.p1
+        
+        d_minus = np.full((R,D), np.inf)
+        d_minus[:, 0:D-1] = E_pre[:, 1:D] + self.p1
+        
+        d_min = np.repeat([np.amin(E_pre, axis=1)], D, axis=0).T + self.p2
+        
+        return np.amin([E_pre, d_plus, d_minus, d_min], axis=0) - (d_min - self.p2)
--- match_cost.py
+++ match_cost.py
@@ -1,59 +1,45 @@
 import numpy as np
+from tqdm import tqdm
 
 class MatchCost():
     def __init__(self, img_L, img_R):
         self.img_L = img_L
         self.img_R = img_R
-        self.window_size = None
-        self.disparity_level = None
-        
     
     def compute(self, disparity_level=5, window_size=3):
-        self.disparity_level = disparity_level
-        self.window_size = window_size
         half_window_size = window_size // 2
+        H, W = self.img_L.shape
         
-        h, w = self.img_L.shape
+        cost_map = np.zeros((H, W, disparity_level), dtype=np.float64)
         
-        disparity_map = np.zeros((h, w, disparity_level), dtype=np.float64)
-        
-        for y in range(half_window_size, (h-half_window_size), 1):
+        for h in tqdm(range(half_window_size, H-half_window_size, 1)):
             
-            for x in range(half_window_size + disparity_level, (w-half_window_size), 1):
+            for w in range(half_window_size+ disparity_level, W-half_window_size, 1):
                 
                 for d in range(disparity_level):
-                    disparity_map[y,x,d] = -self.normalise_cross_correlate(p_L=(x, y), p_R=(x-d, y), window_size=self.window_size)
+                    if w-half_window_size-d >= 0:
+                        cost_map[h,w,d] = -self.normalise_cross_correlate(p_L=(h, w), p_R=(h, w - d), window_size=window_size)
+                    else:
+                        cost_map[h,w,d] = -self.normalise_cross_correlate(p_L=(h, w), p_R=(h, w), window_size=window_size)
         
-        self.disparity_map = np.argmin(disparity_map, axis=2)
-        return self.disparity_map
+        self.cost_map = cost_map
+        self.disparity_map = np.argmin(cost_map, axis=2)
+        
+        return self.disparity_map, self.cost_map
     
     
     def normalise_cross_correlate(self, p_L, p_R, window_size):
-        x_L, y_L = p_L
-        x_R, y_R = p_R
+        h_L, w_L = p_L
+        h_R, w_R = p_R
         
         half_window_size = window_size // 2
         
-        matrix_L = self.img_L[y_L - half_window_size : y_L + half_window_size + 1 , x_L - half_window_size : x_L + half_window_size + 1]
-        matrix_R = self.img_R[y_R - half_window_size : y_R + half_window_size + 1 , x_R - half_window_size : x_R + half_window_size + 1]
+        matrix_L = self.img_L[h_L - half_window_size : h_L + half_window_size + 1, w_L - half_window_size : w_L + half_window_size + 1]
+        matrix_R = self.img_R[h_R - half_window_size : h_R + half_window_size + 1, w_R - half_window_size : w_R + half_window_size + 1]
         
-        nor_matrix_L = self.normalise(matrix_L)
-        nor_matrix_R = self.normalise(matrix_R)
-        
-        return np.sum(nor_matrix_L * nor_matrix_R)
-        
-        
-    def normalise(self, array):
-       
-       mean = np.mean(array, dtype=np.float64)
-       std = np.linalg.norm(array - mean)
-       if std == 0:
-           std = 10**(-8)
-       
-       return np.array((array-mean)/std)
-        
-        
-                
-        
-    
+        matrix_L = matrix_L - np.mean(matrix_L)
+        matrix_R = matrix_R - np.mean(matrix_R)
 
+        norm_L = np.linalg.norm(matrix_L)
+        norm_R = np.linalg.norm(matrix_R)
+        return np.sum((matrix_L * matrix_R))/(norm_L * norm_R) if norm_L and norm_R !=0 else 0
